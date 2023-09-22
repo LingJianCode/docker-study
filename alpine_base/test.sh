9235ca5e9a2350d1fe516cd4a3d15072a7a62b75
ling <lingjian2016@163.com>
2023-09-22 10:07:05 +0800

    docker apline image

---
+++ alpine_base/test.sh
@@ -0,0 +1,3 @@
+echo 'public class Main { public static void main(String[] args) { System.out.println("Java code is running fine!"); } }' > Main.java && \
+    javac Main.java && \
+    java Main && \
